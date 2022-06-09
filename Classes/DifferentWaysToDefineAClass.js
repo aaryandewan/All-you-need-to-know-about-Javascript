773a28ec1a6be7ca14200f34c5e218d147961933
Aaryan Dewan <aaryandewan@yahoo.com>
2022-06-09 17:25:30 +0530

    commit

--- Classes/DifferentWaysToDefineAClass.js
+++ Classes/DifferentWaysToDefineAClass.js
@@ -57,3 +57,32 @@
 // }
 
 //Method 3. Modern way which uses the constructor.
+
+class Range {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+
+  includes(x) {
+    return this.from <= x && x <= this.to;
+  }
+  *[Symbol.iterator]() {
+    for (let x = Math.ceil(this.from); x <= this.to; x++) yield x;
+  }
+  toString() {
+    return `(${this.from}...${this.to})`;
+  }
+}
+
+let r = new Range(1, 50);
+console.log(r.includes(5));
+console.log(r.toString());
+for (let x of r) {
+  console.log(x);
+}
+
+class Person {
+  name = "aara";
+  #age = 12;
+}
